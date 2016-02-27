6df54e8976c58dd23171fb21364484c7a3ca82ba
dphilipson <dphilipson@palantir.com>
2016-02-26 19:00:21 -0800

    Implement state changes in setup

--- src/chronverna/setup.cljs
+++ src/chronverna/setup.cljs
@@ -3,8 +3,39 @@
     [chronverna.constants :as constants]))
 
 (def initial-state
-  {:mode :setup
-   :player-colors (vec (repeat constants/max-players nil))})
+  {:mode    :setup
+   :players [{:name "", :color :red}]})
+
+(defn available-color [state]
+  (let [used-color-set (->> (:players state)
+                            (map :color)
+                            set)]
+    (first (drop-while used-color-set constants/colors))))
+
+(defn add-player [state]
+  (update state :players conj {:name  "",
+                               :color (available-color state)}))
+
+(defn find-indices [pred coll]
+  (keep-indexed #(when (pred %2) %1) coll))
+
+(defn set-player-color-no-swap [state index color]
+  (assoc-in state [:players index :color] color))
 
 (defn set-player-color [state index color]
-  (assoc-in state [:player-colors index] color))
+  (let [current-color (get-in state [:players index :color])
+        old-index (first (find-indices #(= (:color %) color) (:players state)))]
+    (if old-index
+      (-> state
+          (set-player-color-no-swap index color)
+          (set-player-color-no-swap old-index current-color))
+      (set-player-color-no-swap state index color))))
+
+(defn set-player-name [state index name]
+  (assoc-in state [:players index :name] name))
+
+(defn remove-vec-element [v i]
+  (vec (concat (subvec v 0 i) (subvec v (inc i)))))
+
+(defn remove-player [state index]
+  (update state :players remove-vec-element index))
--- test/chronverna/setup_test.cljs
+++ test/chronverna/setup_test.cljs
@@ -3,20 +3,64 @@
     [cljs.test :refer-macros [deftest testing is]]
     [chronverna.setup :as setup]))
 
+(defn setup-state [players]
+  {:mode :setup, :players players})
+
+; Test players
+
+(defn anonymous [color]
+  {:name "", :color color})
+
+(def ms-scarlet
+  {:name "Miss Scarlet", :color :red})
+
+(def mr-green
+  {:name "Mr. Green", :color :green})
+
+(def prof-plum
+  {:name "Professor Plum", :color :purple})
+
 (deftest test-initial-state
-  (testing "It should initialize with all players absent"
-    (is (= setup/initial-state {:mode          :setup
-                                :player-colors [nil nil nil nil nil nil nil]}))))
+  (testing "It should initialize with one player"
+    (is (= setup/initial-state (setup-state [(anonymous :red)])))))
+
+(deftest test-add-player
+  (testing "It should add a new player"
+    (let [initial-state (setup-state [mr-green prof-plum])
+          updated-state (setup/add-player initial-state)
+          expected-state (setup-state [mr-green prof-plum (anonymous :red)])]
+      (is (= updated-state expected-state))))
+  (testing "It should choose an available color"
+    (let [initial-state (setup-state [ms-scarlet])
+          updated-state (setup/add-player initial-state)
+          expected-state (setup-state [ms-scarlet (anonymous :orange)])]
+      (is (= updated-state expected-state)))))
 
 (deftest test-set-player-color
-  (testing "It should set the color for the selected player"
-    (is (= (setup/set-player-color setup/initial-state 1 :green)
-           {:mode          :setup
-            :player-colors [nil :green nil nil nil nil nil]})))
-  (testing "It should set the color to nil if requested"
-    (let [initial-state {:mode    :setup
-                         :player-colors [:red :green :blue nil nil nil nil]}
-          updated-state (setup/set-player-color initial-state 1 nil)
-          expected-state {:mode :setup
-                          :player-colors [:red nil :blue nil nil nil nil]}]
+  (testing "It should set a player's color"
+    (let [initial-state (setup-state [mr-green prof-plum])
+          updated-state (setup/set-player-color initial-state 1 :red)
+          expected-state (setup-state [mr-green
+                                       {:name "Professor Plum", :color :red}])]
+      (is (= updated-state expected-state))))
+  (testing "It should swap colors if selection is already used"
+    (let [initial-state (setup-state [mr-green prof-plum])
+          updated-state (setup/set-player-color initial-state 1 :green)
+          expected-state (setup-state [{:name "Mr. Green", :color :purple}
+                                       {:name "Professor Plum", :color :green}])]
+      (is (= updated-state expected-state)))))
+
+(deftest test-set-player-name
+  (testing "It should set a player's name"
+    (let [initial-state (setup-state [mr-green prof-plum])
+          updated-state (setup/set-player-name initial-state 1 "Christian Grey")
+          expected-state (setup-state [mr-green
+                                       {:name "Christian Grey", :color :purple}])]
+      (is (= updated-state expected-state)))))
+
+(deftest test-remove-player
+  (testing "It should remove a player"
+    (let [initial-state (setup-state [ms-scarlet mr-green prof-plum])
+          updated-state (setup/remove-player initial-state 1)
+          expected-state (setup-state [ms-scarlet prof-plum])]
       (is (= updated-state expected-state)))))
